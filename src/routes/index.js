137ce378816102755fb06fe4281cc14ed1cd8b5f
Orianna Milone <oriannamilone.s@gmail.com>
2024-12-08 16:08:10 +0100

    login y registro de usuarios super cutre pero funcional

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,34 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('../chatingApp.db');
 
+//Middleware Functions
+function checkUserExistence(email){
+  return new Promise((resolve, reject) => {
+    const sqlCheck = "SELECT email FROM users WHERE email = ?"
+    db.get(sqlCheck, [email], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
+  }); 
+};
+
+function registerUser(name, email, password){
+  return new Promise((resolve, reject) => {
+    const registryQuery = "INSERT INTO users (name, email, password) VALUES (?,?,?)";
+    db.run(registryQuery, [name, email, password], (err) => {
+      if (err) reject (err);
+      else resolve ();
+    });
+  });
+}
+
+
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
 
 router.post("/loginUserDDBB", function(req, res){
     let useremail = req.body.email;
@@ -28,6 +52,27 @@ router.post("/loginUserDDBB", function(req, res){
       }
     });
     res.status(200).send("User logged in");
+});
+
+router.post("/registerUserDDBB", async (req, res) => {
+  const username = req.body.name;
+  const useremail = req.body.email;
+  const userpassword = req.body.password;
+
+  try {
+    const userExist = await checkUserExistence(useremail);
+
+    if (userExist) {
+      console.log("The user is already registered");
+      res.status(400).send("The user is already registered");
+    }  
+    await registerUser(username, useremail, userpassword);
+    res.status(200).send("User registered successfully.");
+    
+  } catch (err) {
+    console.error("Error processing registration:", err.message);
+    res.status(500).send("Server error.");
+  }
 });
 
 module.exports = router;
